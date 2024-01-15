ae9e46441260c88aabe4063db7b59cc8516f86af
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-01-15 12:32:30 +0530

    changes

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -31,7 +31,7 @@ function Form() {
         </div>
         <div className='flex justify-center items-center'>
         <button class=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-3 border border-blue-500 hover:border-transparent rounded">
-  Button
+  Submit
 </button>
 </div>
       </form>
